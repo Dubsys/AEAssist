8ca5872b51f44701800676039b08bb138f51ad77
zfxsquare <zfxsquare@outlook.com>
2022-08-31 22:31:08 +0800

    update dnc

---
+++ AEAssist/AI/Dancer/GCD/DancerGCD_DanceStep2.cs
@@ -0,0 +1,53 @@
+﻿using AEAssist.Define;
+using AEAssist.Helper;
+using ff14bot.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AEAssist.AI.Dancer.GCD
+{
+    internal class DancerGCD_DanceStep2 : IAIHandler
+    {
+        public int Check(SpellEntity lastSpell)
+        {
+            return 0;
+        }
+
+        public async Task<SpellEntity> Run()
+        {
+            if (ActionResourceManager.Dancer.CurrentStep == ActionResourceManager.Dancer.DanceStep.Finish)
+            {
+                return null;
+            }
+            var spell = SpellsDefine.Cascade.GetSpellEntity();
+            var spellnum = ActionResourceManager.Dancer.CurrentStep;
+            switch (spellnum)
+            {
+                case ActionResourceManager.Dancer.DanceStep.Pirouette:
+                    spell = SpellsDefine.Pirouette.GetSpellEntity();
+                    break;
+                case ActionResourceManager.Dancer.DanceStep.Emboite:
+                    spell = SpellsDefine.Emboite.GetSpellEntity();
+                    break;
+                case ActionResourceManager.Dancer.DanceStep.Entrechat:
+                    spell = SpellsDefine.Entrechat.GetSpellEntity();
+                    break;
+                case ActionResourceManager.Dancer.DanceStep.Jete:
+                    spell = SpellsDefine.Jete.GetSpellEntity();
+                    break;
+                default:
+                    spell = null;
+                    break;
+            }
+            if (spell == null)
+                return null;
+            var ret = await spell.DoGCD();
+            if (ret)
+                return spell;
+            return null;
+        }
+    }
+}
