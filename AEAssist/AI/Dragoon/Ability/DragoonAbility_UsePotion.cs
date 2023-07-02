97e3d3817f2f8102dd54e1ddd50bddf6e82b87a5
Nick <2124370+Dubsys@users.noreply.github.com>
2023-07-02 23:20:51 +1000

    Create DragoonAbility_UsePotion.cs

---
+++ AEAssist/AI/Dragoon/Ability/DragoonAbility_UsePotion.cs
@@ -0,0 +1,35 @@
+﻿using AEAssist.Define;
+using AEAssist.Helper;
+using ff14bot;
+using ff14bot.Managers;
+using ff14bot.Objects;
+using System.Threading.Tasks;
+
+namespace AEAssist.AI.Dragoon.Ability
+{
+    public class DragoonAbility_UsePotion : IAIHandler
+    {
+        public int Check(SpellEntity lastSpell)
+        {
+            if (!SettingMgr.GetSetting<GeneralSettings>().UsePotion)
+                return -1;
+            if (AIRoot.Instance.CloseBurst)
+                return -2;
+            if (TTKHelper.IsTargetTTK(Core.Me.CurrentTarget as Character))
+                return -3;
+            if (!PotionHelper.CheckPotion(SettingMgr.GetSetting<GeneralSettings>().StrPotionId))
+                return -5;
+
+            return 0;
+        }
+
+        public async Task<SpellEntity> Run()
+        {
+            var ret = await PotionHelper.UsePotion(SettingMgr.GetSetting<GeneralSettings>().StrPotionId);
+            if (ret) AIRoot.Instance.MuteAbilityTime();
+
+            await Task.CompletedTask;
+            return null;
+        }
+    }
+}
